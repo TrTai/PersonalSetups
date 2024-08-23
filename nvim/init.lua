c3e7f07df760e18b04891453f4f4c48ecbbcf5c7
TrTai <38041450+TrTai@users.noreply.github.com>
2024-08-22 23:22:46 -0400

    Update init.lua LSPs
    added bash lua and terraform LSPs for Mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -537,6 +537,11 @@ require('lazy').setup {
         --rust_analyzer = {},
         ansiblels = {},
         powershell_es = {},
+        bashls = {},
+        lua_ls = {},
+        terraformls = {},
+        
+        
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
