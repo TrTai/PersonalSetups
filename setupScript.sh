c63e9902f9a5a2cc1b393e4256f2c66141612700
TrTai <chaney.t@live.com>
2024-08-24 01:05:59 -0400

    seperated out user and system scripts

--- setupScript.sh
+++ setupScript.sh
@@ -18,25 +18,11 @@ if [[ $VIRTUALIZATION =~ "VT-x" ]] || [[ $VIRTUALIZATION =~ "AMD-V" ]]; then
 	APTINSTALLLIST+=" quickemu virtualbox"
 	IFVIRTUALIZATION=true
 fi
-VERSION=$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
-VERSION+=".linux-amd64.tar.gz"
-$(wget https://go.dev/dl/$VERSION)
-$(rm -rf /usr/local/go && tar -C /usr/local -xzf $VERSION)
-#echo "GOLANG installed"
-echo 'export PATH="$PATH":/usr/local/go/bin' >> ~/.bashrc
-#echo "GOLANG added to PATH"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
 #echo "Chrome installed"
 bash <(curl -sL https://github.com/xpipe-io/xpipe/raw/master/get-xpipe.sh)
-#echo "nvim config updated"
 nala fetch --auto --fetches 5
 nala update
 $(nala install $APTINSTALLLIST -y)
-wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
-#echo "installed pip" 
-#python3 -m ensurepip --upgrade
-python3 -m pip install --user --upgrade pynvim
-git clone https://github.com/TrTai/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
