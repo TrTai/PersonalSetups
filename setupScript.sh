36699627f274ff105b4b78759d34ed9cea30947e
TrTai <38041450+TrTai@users.noreply.github.com>
2024-08-23 23:50:22 -0400

    Update setupScript.sh
    readded MS edge ppa 
    added echo comments for stages
    added nala fetch list 
    added -y options for non-interactive mode
    added 'virtualization enabled' feedback to verify state

--- setupScript.sh
+++ setupScript.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 sed -i 's/WaylandEnable=false/WaylandEnable=true/g' /etc/gdm3/custom.conf
-#IFVIRTUALIZATION=false
+IFVIRTUALIZATION=false
 add-apt-repository ppa:neovim-ppa/unstable -y
 add-apt-repository ppa:flexiondotorg/quickemu -y
-#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
 #install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
-#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list
 curl -fsSL https://ppa.ablaze.one/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
 curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 dpkg -i ./packages-microsoft-prod.deb
 apt update
-apt-get install nala
+apt-get install nala -y
 VIRTUALIZATION=$(lscpu | grep Virtualization)
 APTINSTALLLIST="tmux python3 make gcc ripgrep unzip git xclip neovim cosmic-session remmina remmina-plugin-rdp remmina-plugin-secret remmina-dev floorp"
 if [[ $VIRTUALIZATION =~ "VT-x" ]] || [[ $VIRTUALIZATION =~ "AMD-V" ]]; then
 	APTINSTALLLIST+=" quickemu virtualbox"
-	#IFVIRTUALIZATION=true
+	IFVIRTUALIZATION=true
+fi
+if [[ $IFVIRTUALIZATION = true]]; then
+	echo "Virtualization enabled"
 fi
 VERSION=$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
 VERSION+=".linux-amd64.tar.gz"
 $(wget https://go.dev/dl$VERSION)
 $(rm -rf /usr/local/go && tar -C /usr/local -xzf $VERSION)
+echo "GOLANG installed"
 echo 'export PATH="$PATH":/usr/local/go/bin' >> ~/.bashrc
+echo "GOLANG added to PATH"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
+echo "Chrome installed"
 bash <(curl -sL https://github.com/xpipe-io/xpipe/raw/master/get-xpipe.sh)
 git clone https://github.com/TrTai/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+echo "nvim config updated"
+nala fetch --auto --fetches 5
 $(nala install $APTINSTALLLIST -y)
 wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
+echo "installed pip" 
 python3 -m ensurepip --upgrade
 python3 -m pip install --user --upgrade pynvim
 
