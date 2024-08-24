4642fcd940d7f8441f4a4e3fbd1b144aaeda8e35
TrTai <38041450+TrTai@users.noreply.github.com>
2024-08-24 01:48:44 -0400

    Update systemScript.sh
    moved microsoft ppa deb install after other apt installs to prevent dpkg lock for time period

--- systemScript.sh
+++ systemScript.sh
@@ -8,8 +8,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/s
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list
 curl -fsSL https://ppa.ablaze.one/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
-curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
-dpkg -i ./packages-microsoft-prod.deb
 apt update
 VIRTUALIZATION=$(lscpu | grep Virtualization)
 APTINSTALLLIST="tmux python3 make gcc ripgrep unzip git xclip neovim cosmic-session remmina remmina-plugin-rdp remmina-plugin-secret remmina-dev floorp microsoft-edge-stable nala"
@@ -20,6 +18,8 @@ fi
 $(apt install $APTINSTALLLIST -y)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+dpkg -i ./packages-microsoft-prod.deb
 #echo "Chrome installed"
 VERSION=$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
 VERSION+=".linux-amd64.tar.gz"
