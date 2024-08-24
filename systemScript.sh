88616885fedafd35843ccd2b124e30b37e600f5c
TrTai <chaney.t@live.com>
2024-08-24 13:44:40 -0400

    Added echo results

--- systemScript.sh
+++ systemScript.sh
@@ -6,15 +6,19 @@ add-apt-repository ppa:flexiondotorg/quickemu -y
 if [ ! -f /usr/share/keyrings/microsoft.gpg ]; then
 	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
 	#install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+	echo "add Microsoft key"
 fi
 if [ ! -f /etc/apt/sources.list.d/microsoft-edge-beta.list ]; then
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list
+	echo "Added Microsoft Edge Source"
 fi
 if [ ! -f /usr/share/keyrings/Floorp.gpg ]; then
 	curl -fsSL https://ppa.ablaze.one/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+	echo "Added Floorp Key"
 fi
 if [ ! -f /etc/apt/sources.list.d/Floorp.list]; then
 	curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+	echo "Added Floorp Source"
 fi
 apt-get update
 VIRTUALIZATION=$(lscpu | grep Virtualization)
@@ -32,6 +36,7 @@ fi
 if [ ! $(apt list --installed) =~ "packages-microsoft.prod" ]; then
 	curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 	dpkg -i ./packages-microsoft-prod.deb
+	echo "Microsoft ppa installed"
 fi
 VERSION=$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
 if [[ ! $(go version) =~ $VERSION ]]; then
