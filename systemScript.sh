66b6c473d79dad67e697e069ec9341d52e5f98ce
TrTai <chaney.t@live.com>
2024-08-25 01:23:16 -0400

    Updated package list with media, qemu and readded quickemi

--- systemScript.sh
+++ systemScript.sh
@@ -11,7 +11,7 @@ if [ ! -f /usr/share/keyrings/microsoft.gpg ]; then
 fi
 if [ ! -f /etc/apt/sources.list.d/microsoft-edge-beta.list ]; then
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list
-	echo "Added Microsoft Edge Source"
+	echo "Added Microsoft Edge apt Source"
 fi
 if [ ! -f /usr/share/keyrings/Floorp.gpg ]; then
 	curl -fsSL https://ppa.ablaze.one/KEY.gpg | gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
@@ -19,17 +19,19 @@ if [ ! -f /usr/share/keyrings/Floorp.gpg ]; then
 fi
 if [ ! -f /etc/apt/sources.list.d/Floorp.list ]; then
 	curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
-	echo "Added Floorp Source"
+	echo "Added Floorp apt Source"
 fi
 if [ ! -f /etc/apt/keyrings/docker.asc ]; then
 	curl -fsSl https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	echo "Added Docker keyring"
 fi
 if [ ! -f /etc/apt/sources.list.d/docker.list ]; then
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	echo "Added Docker apt source"
 fi
 apt-get update
 VIRTUALIZATION=$(lscpu | grep Virtualization)
-APTINSTALLLIST="tmux python3 make gcc ripgrep unzip git xclip neovim remmina remmina-plugin-rdp remmina-plugin-secret remmina-dev floorp microsoft-edge-stable nala python3-venv python3-pip google-chrome-stable code kitty docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin distrobox"
+APTINSTALLLIST="tmux python3 make gcc ripgrep unzip git xclip ubuntu-restricted-extras neovim qemu-system quickemu remmina remmina-plugin-rdp remmina-plugin-secret remmina-dev floorp microsoft-edge-stable nala python3-venv python3-pip google-chrome-stable code kitty docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin distrobox ffmpeg gimp obs-studio"
 if [[ $VIRTUALIZATION =~ "VT-x" ]] || [[ $VIRTUALIZATION =~ "AMD-V" ]]; then
 	APTINSTALLLIST+=" quickemu virtualbox"
 	IFVIRTUALIZATION=true
